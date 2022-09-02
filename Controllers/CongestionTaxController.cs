1f9be75d783ba270df91b17f6e80d8186720dc29
Admire Khulumo <admirekhulumo@gmail.com>
2022-09-02 09:32:42 +0200

    remove test string

--- Controllers/CongestionTaxController.cs
+++ Controllers/CongestionTaxController.cs
@@ -36,8 +36,7 @@ namespace CongestionTax.Controllers
         [HttpPost]
         public async Task<Dictionary<string, double>> GetAsync(string city, string type, string datesStr)
         {
-            string test = "2013-02-08 06:20:27,2013-02-08 06:27:00, 2013-02-08 14:35:00 , 2013-02-08 15:29:00 , 2013-02-08 15:47:00 , 2013-02-08 16:01:00 , 2013-02-08 16:48:00,2013-02-08 17:49:00 ,  2013-02-08 17:55:00,2013-02-08 18:01:00, 2013-02-08 18:29:00, 2013-02-08 18:35:00, 2013-02-08 18:40:00";
-
+        
             // create a vehicle object 
             Vehicle veh = createVehObj(type);
 
